abfde1b327ec3c05d1030ff049276b3e103fff77
Andy OConnell <andrewoconnell89@gmail.com>
2017-09-07 11:17:00 -0400

    Finished Insertion Sort with Tests

--- Sorting/InsertionSort.py
+++ Sorting/InsertionSort.py
@@ -1,33 +1,89 @@
 import numpy as np
 import datetime as dt
 
-
 """
 Insertion Sort 
 """
 
 
-def sort_iterative(arr, arr, i):
-    """Sorts the array Iteratavly"""
-    for i in range(len(arr)):
-        key = arr[j]
+def sort_iterative(arr, n):
+    """Sorts the array using Insertion Sort iteratively.
+    
+    Params:
+        arr: the array to be sorted
+        n: the number of elements to be sorted starting from the left
+    """
+    for i in range(0, n):
+        key = arr[i] 
         j = i - 1
+        
         # Insert arr[i] into sorted sequence arr[1 .. i-1]
-        while j > 0 and arr[j] > key:
+        while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
-    return arr
 
-def sort_recursive(self, arr, i):
-    return None
-
+def sort_recursive(arr, n):
+    """Sorts the array using Insertion Sort Recursively
+    
+    Params:
+        arr: the array to be sorted
+        n: the number of elements to be sorted starting from the left
+    """
+    # Base Case will do nothing
+    if n <=1:
+        return None
+    
+    sort_recursive(arr, n-1)
+    i = n-1
+    j = n-2
+    key = arr[i]
+    while j >= 0 and arr[j] > key:
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j + 1] = key
 
 def test_1():
-    data = np.random.randn(100)
+    data = [5, 3, 2, 7, 4, 6]
+    answer = [2, 3, 4, 5, 6, 7]
+    data_size = len(data)
 
-    s = InsertionSort(data)
-    result = s.sort()
-    print(result)
+    sort_recursive(data, data_size)
+
+    success = answer == data
+    print("Test 1: %s" % success)
+
+def test_2():
+    data = [5, 3, 2, 7, 4, 6]
+    answer = [2, 3, 4, 5, 6, 7]
+    data_size = len(data)
+
+    sort_iterative(data, data_size)
+
+    success = answer == data
+    print("Test 2: %s" % success)
+
+def test_3():
+    data = [3, 2]
+    answer = [2, 3]
+    data_size = len(data)
+
+    sort_iterative(data, data_size)
+
+    success = answer == data
+    print("Test 3: %s" % success)
+
+def test_4():
+    data = [3, 2]
+    answer = [2, 3]
+    data_size = len(data)
+
+    sort_recursive(data, data_size)
+
+    success = answer == data
+    print("Test 4: %s" % success)
 
 test_1()
+test_2()
+test_3()
+test_4()
